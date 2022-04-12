ce20663aa410087d9200903be47bafd1a5f27100
Juan Gallardo <juangallardovergara17@gmail.com>
2022-04-11 21:52:47 -0400

    Agregado la vista para el usuario de los planes

--- client/src/auth/helpers/routes.js
+++ client/src/auth/helpers/routes.js
@@ -1,10 +1,11 @@
 const routes ={
     //rutas a usar
+    //Planes equivale a la vista que verán los usuarios donde estarán los planes
     home:'/',
     users:'/users',
     login:'users/login',
     plan:'/plan',
-    
+    planes: '/planes'
 }
 
 export default routes;
