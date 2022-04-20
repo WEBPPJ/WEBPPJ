91f4a20a6b1bd8ae22a6735baf954e7f3c62acc7
BastianAcuna <BastianAcS@hotmail.com>
2022-04-20 10:34:13 -0400

    link controller

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('./database')
 const express = require('express')
 const app = express()
 const userRouter=require('./routes/userRoutes')
+const linkRouter=require('./routes/LinkRoutes')
 app.use(express.json())
 
 
@@ -11,6 +12,7 @@ app.use(cors())
 
 //rutas de usuarios
 app.use("/api/users", userRouter)
+app.use("/api/links", linkRouter)
 
 
 app.listen(3001, () => {
